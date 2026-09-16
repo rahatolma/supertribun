ba41c4c0f873b86c1e20abaf3c708899914952c4
rahatolma <rahatolma@gmail.com>
2026-09-16 15:58:54 +0300

    Arkaplan

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Prevent right click and copying
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('copy', event => event.preventDefault());
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
